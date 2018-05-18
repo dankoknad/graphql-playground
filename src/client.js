17d7cc37260d5bc90338846477ba2bbf8101d6b0
dankoknad <dankomilutinovic@gmail.com>
2018-05-18 23:44:28 +0200

    adding Apollo Client Developer Tools

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
+  connectToDevTools: true,
   // uri: "https://w5xlvm3vzz.lp.gql.zone/graphql" // ExchangeRates
   uri: "https://nx9zvp49q7.lp.gql.zone/graphql" // Dogs
 });
