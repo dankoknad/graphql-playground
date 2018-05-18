721e2f064c17bed19544df07c5c0ba79066b34d6
dankoknad <dankomilutinovic@gmail.com>
2018-05-18 20:13:28 +0200

    switching to different graphql api, making new query, rendering select/options with results

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { ApolloProvider } from "react-apollo";
-import ExchangeRates from './ExchangeRates'
+import Dogs from './Dogs'
+import DogPhoto from './DogPhoto'
+// import ExchangeRates from './ExchangeRates'
 import client from './client'
 
 class App extends Component {
@@ -9,7 +11,9 @@ class App extends Component {
       <ApolloProvider client={client}>
         <div className="text-center">
           <h2>My first Apollo app <span role="img" aria-label="emoji rocket">🚀</span></h2>
-          <ExchangeRates />
+          <Dogs />
+          <DogPhoto />
+          {/* <ExchangeRates /> */}
         </div>
       </ApolloProvider>
     );
--- src/client.js
+++ src/client.js
@@ -1,7 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  // uri: "https://w5xlvm3vzz.lp.gql.zone/graphql" // ExchangeRates
+  uri: "https://nx9zvp49q7.lp.gql.zone/graphql" // Dogs
 });
 
 export default client;
